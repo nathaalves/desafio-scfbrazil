55daf99eb3c50154cdf5738b046fa027ab4752f9
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-06-14 19:04:40 -0300

    feat: create fake data

---
+++ src/data/fakeData.ts
@@ -0,0 +1,11 @@
+import { UserType } from '../types/userTypes';
+
+const fakeData: UserType[] = [
+  {
+    id: 1,
+    name: 'João Oliveira',
+    job: 'Desenvolvedor',
+  },
+];
+
+export default fakeData;
